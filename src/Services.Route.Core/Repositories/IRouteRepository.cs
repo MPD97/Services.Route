292cc696ba1130495b07a79d96189696c8ab162d
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-19 23:46:35 +0200

    created repository

---
+++ src/Services.Route.Core/Repositories/IRouteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Services.Route.Core.Repositories
+{
+    public interface IRouteRepository
+    {
+        Task<Entities.Route> GetAsync(Guid id);
+        Task AddAsync(Entities.Route route);
+        Task UpdateAsync(Entities.Route route);
+    }
+}
