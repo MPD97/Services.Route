e74bd7c82d4efeb350488b2c6dbebc69b7b2d258
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-19 23:46:10 +0200

    Created value object

---
+++ src/Services.Route.Core/ValueObjects/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Services.Route.Core.ValueObjects
+{
+    public struct Point: IEquatable<Point>
+    {
+        public Guid Id { get; }
+        public int Order { get; }
+        public decimal Latitude { get; }
+        public decimal Longitude { get; }
+        public int Radius { get; }
+
+        
+        public bool Equals(Point other)
+        {
+            return Id.Equals(other.Id) && Order == other.Order && Latitude == other.Latitude && Longitude == other.Longitude && Radius == other.Radius;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Point other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Order, Latitude, Longitude, Radius);
+        }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
