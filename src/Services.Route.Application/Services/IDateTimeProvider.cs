f256e5f21e243df98f6b6e12a22c94922a4286dd
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-20 16:03:45 +0200

    Created services

---
+++ src/Services.Route.Application/Services/IDateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Services.Route.Application.Services
+{
+    public interface IDateTimeProvider
+    {
+        DateTime Now { get; }
+    }
+}
---
+++ src/Services.Route.Application/Services/IDistanceMeasure.cs
@@ -0,0 +1,7 @@
+﻿namespace Services.Route.Application.Services
+{
+    public interface IDistanceMeasure
+    {
+        double GetDistanceBetween(decimal lat1, decimal lon1, decimal lat2, decimal lon2);
+    }
+}
