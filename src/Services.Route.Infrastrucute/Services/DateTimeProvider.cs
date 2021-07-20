f1b862f859b4f97fed2a99e5230f1f8c7b21a4ea
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-20 17:05:06 +0200

    Created service implementations

---
+++ src/Services.Route.Infrastrucute/Services/DateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Services.Route.Application.Services;
+
+namespace Services.Route.Infrastrucute.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime Now  => DateTime.UtcNow;
+    }
+}
---
+++ src/Services.Route.Infrastrucute/Services/EventMapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Convey.CQRS.Events;
+using Services.Route.Application.Services;
+using Services.Route.Core;
+using Services.Route.Core.Events;
+
+namespace Services.Route.Infrastrucute.Services
+{
+    public class EventMapper : IEventMapper
+    {
+        public IEnumerable<IEvent> MapAll(IEnumerable<IDomainEvent> events)
+            => events.Select(Map);
+
+        public IEvent Map(IDomainEvent @event)
+        {
+            switch (@event)
+            {
+                case RouteCreated e: return new Application.Events.RouteCreated(e.Route);
+            }
+
+            return null;
+        }
+    }
+}
