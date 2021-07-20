3cc804d0f1f73d59ed5685e490aefe8963d13ea8
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-20 21:32:05 +0200

    Created handlers

---
+++ src/Services.Route.Infrastructure/Mongo/Queries/Handlers/GetRouteHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using Services.Route.Application.DTO;
+using Services.Route.Application.Queries;
+using Services.Route.Infrastructure.Mongo.Documents;
+
+namespace Services.Route.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetRouteHandler: IQueryHandler<GetRoute, RouteDto>
+    {
+        private readonly IMongoRepository<RouteDocument, Guid> _routeDocument;
+
+        public GetRouteHandler(IMongoRepository<RouteDocument, Guid> routeDocument)
+        {
+            _routeDocument = routeDocument;
+        }
+
+        public async Task<RouteDto> HandleAsync(GetRoute query)
+        {
+            var document = await _routeDocument.GetAsync(p => p.Id == query.RouteId);
+
+            return document?.AsDto();
+        }
+    }
+}
---
+++ src/Services.Route.Infrastructure/Mongo/Queries/Handlers/GetRoutesHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using Services.Route.Application.DTO;
+using Services.Route.Application.Queries;
+using Services.Route.Infrastructure.Mongo.Documents;
+
+namespace Services.Route.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetRoutesHandler: IQueryHandler<GetRoutes, IEnumerable<RouteDto>>
+    {
+        private readonly IMongoRepository<RouteDocument, Guid> _routeDocument;
+
+        public GetRoutesHandler(IMongoRepository<RouteDocument, Guid> routeDocument)
+        {
+            _routeDocument = routeDocument;
+        }
+
+        public async Task<IEnumerable<RouteDto>> HandleAsync(GetRoutes query)
+        {
+            var routes = await _routeDocument.FindAsync(_ => true);
+
+            return routes.Select(r => Documents.Extensions.AsDto(r));
+        }
+    }
+}
