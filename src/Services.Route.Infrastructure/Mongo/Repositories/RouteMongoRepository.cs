834f09ccb07d79bdc93869d8702e2ce1664193cb
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-20 21:31:58 +0200

    Created repositories

---
+++ src/Services.Route.Infrastructure/Mongo/Repositories/RouteMongoRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.Persistence.MongoDB;
+using Services.Route.Core.Repositories;
+using Services.Route.Infrastructure.Mongo.Documents;
+
+namespace Services.Route.Infrastructure.Mongo.Repositories
+{
+    public class RouteMongoRepository : IRouteRepository
+    {
+        private readonly IMongoRepository<RouteDocument, Guid> _repository;
+
+        public RouteMongoRepository(IMongoRepository<RouteDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+
+        public async Task<Core.Entities.Route> GetAsync(Guid id)
+        {
+            var route = await _repository.GetAsync(o => o.Id == id);
+
+            return route?.AsEntity();
+        }
+
+        public Task AddAsync(Core.Entities.Route route) => _repository.AddAsync(route.AsDocument());
+
+        public Task UpdateAsync(Core.Entities.Route route) => _repository.UpdateAsync(route.AsDocument());
+    }
+}
---
+++ src/Services.Route.Infrastructure/Mongo/Repositories/UserMongoRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.Persistence.MongoDB;
+using Services.Route.Core.Repositories;
+using Services.Route.Infrastructure.Mongo.Documents;
+
+namespace Services.Route.Infrastructure.Mongo.Repositories
+{
+    public class UserMongoRepository : IUserRepository
+    {
+        private readonly IMongoRepository<UserDocument, Guid> _repository;
+
+        public UserMongoRepository(IMongoRepository<UserDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+            => await _repository.ExistsAsync(u => u.Id == id);
+
+        public async Task<Core.Entities.User> GetAsync(Guid id)
+        {
+            var user = await _repository.GetAsync(u => u.Id == id);
+
+            return user?.AsEntity();
+        }
+
+        public Task AddAsync(Core.Entities.User user) => _repository.AddAsync(user.AsDocument());
+        public Task UpdateAsync(Core.Entities.User user) => _repository.UpdateAsync(user.AsDocument());
+    }
+}
