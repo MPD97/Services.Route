7adfe710bbd6b6cfcc6af1642797ae02ad6740a6
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-20 16:04:34 +0200

    Created command

---
+++ src/Services.Route.Application/Commands/CreateRoute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Convey.CQRS.Commands;
+using Services.Route.Core.Entities;
+
+namespace Services.Route.Application.Commands
+{
+    [Contract]
+    public class CreateRoute : ICommand
+    {
+        public Guid RouteId { get; }
+        public Guid UserId { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public string Difficulty { get; }
+        public ActivityKind ActivityKind  { get; }
+        public IEnumerable<Point> Points { get; }
+
+        public CreateRoute(Guid routeId, Guid userId, string name, string description, string difficulty, ActivityKind activityKind, IEnumerable<Point> points)
+        {
+            RouteId = routeId == Guid.Empty ? Guid.NewGuid() : routeId;
+            UserId = userId;
+            Name = name;
+            Description = description;
+            Difficulty = difficulty;
+            ActivityKind = activityKind;
+            Points = points;
+        }
+        public class Point
+        {
+            public Guid PointId { get; }
+            public int Order { get; }
+            public decimal Latitude { get; }
+            public decimal Longitude { get; }
+            public int Radius { get; }
+
+            public Point(Guid pointId, int order, decimal latitude, decimal longitude, int radius)
+            {
+                PointId = pointId == Guid.Empty ? Guid.NewGuid() : pointId;
+                Order = order;
+                Latitude = latitude;
+                Longitude = longitude;
+                Radius = radius;
+            }
+        }
+    }
+}
---
+++ src/Services.Route.Application/Commands/Handlers/CreateRouteHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Convey.CQRS.Commands;
+using Services.Route.Application.Events;
+using Services.Route.Application.Exceptions;
+using Services.Route.Application.Services;
+using Services.Route.Core.Entities;
+using Services.Route.Core.Repositories;
+using Services.Route.Core.ValueObjects;
+
+namespace Services.Route.Application.Commands.Handlers
+{
+    public class CreateRouteHandler : ICommandHandler<CreateRoute>
+    {
+        private readonly IRouteRepository _routeRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageBroker _messageBroker;
+        private readonly IDistanceMeasure _distanceMeasure;
+
+        public CreateRouteHandler(IRouteRepository routeRepository, IUserRepository userRepository,
+             IMessageBroker messageBroker, IDistanceMeasure distanceMeasure)
+        {
+            _routeRepository = routeRepository;
+            _userRepository = userRepository;
+            _messageBroker = messageBroker;
+            _distanceMeasure = distanceMeasure;
+        }
+
+        public async Task HandleAsync(CreateRoute command)
+        {
+            var user = await _userRepository.GetAsync(command.UserId);
+            if (user is null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            if (user.State != State.Valid)
+            {
+                throw new InvalidUserStateException(command.UserId, user.State);
+            }
+            
+            if (!Enum.TryParse<Difficulty>(command.Difficulty, true, out var difficulty))
+            {
+                throw new InvalidRouteDifficultyException(command.UserId, command.Difficulty);
+            }
+
+            const int minPointsCount = 3;
+            if (command.Points is null)
+            {
+                throw new InvalidRoutePointsCountException(command.UserId, minPointsCount, 0);
+            }
+            
+            var pointsCount = command.Points.Count();
+            if (pointsCount is < minPointsCount)
+            {
+                throw new InvalidRoutePointsCountException(command.UserId, minPointsCount, pointsCount);
+            }
+
+            var points = new SortedSet<Point>(new ByOrderExtension());
+            foreach (var point in command.Points)
+            {
+                points.Add(new Point(point.PointId, point.Order, point.Latitude, point.Longitude, point.Radius));
+            }
+
+            const int minDistance = 100;
+            const int maxDistance = 500;
+            for (int i = 0; i < points.Count; i++)
+            {
+                var currentPoint = points.ElementAt(i);
+                if (currentPoint.Order != i)
+                {
+                    throw new InvalidPointOrderException(command.UserId, 
+                        string.Join(", ",points.Select(p => p.Order)));
+                }
+
+                const int minRadius = 10;
+                const int maxRadius = 50;
+                if (currentPoint.Radius is < minRadius or > maxRadius)
+                {
+                    throw new InvalidPointRadiusException(command.UserId, currentPoint.Radius,
+                        minRadius, maxRadius);
+                }
+                
+                if (i <= 0) continue;
+                
+                var previousPoint = points.ElementAt(i - 1);
+                var distance = _distanceMeasure.GetDistanceBetween(previousPoint.Latitude,
+                    previousPoint.Longitude, currentPoint.Latitude, currentPoint.Longitude);
+
+                if (distance is < minDistance or > maxDistance)
+                {
+                    throw new InvalidDistanceBetweenPointsException(command.UserId, previousPoint,
+                        currentPoint, distance, minDistance, maxDistance);
+                }
+            }
+            
+            var route = new Core.Entities.Route(command.RouteId, command.UserId, command.Name, command.Description,
+                difficulty, Status.New, points, command.ActivityKind);
+
+            await _routeRepository.AddAsync(route);
+            await _messageBroker.PublishAsync(new RouteCreated(route));
+        }
+    }
+}
