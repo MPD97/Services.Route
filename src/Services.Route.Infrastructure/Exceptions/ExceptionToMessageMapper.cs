7716b316e426af93623376e63c99ce8690d518eb
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-20 21:31:33 +0200

    Created exception mappers

---
+++ src/Services.Route.Infrastructure/Exceptions/ExceptionToMessageMapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Convey.MessageBrokers.RabbitMQ;
+using Services.Route.Application.Events.Rejected;
+using Services.Route.Application.Exceptions;
+using Services.Route.Core.Exceptions;
+using InvalidPointOrderException = Services.Route.Application.Exceptions.InvalidPointOrderException;
+
+namespace Services.Route.Infrastructure.Exceptions
+{
+    public class ExceptionToMessageMapper : IExceptionToMessageMapper
+    {
+        public object Map(Exception exception, object message)
+            => exception switch
+            {
+                UserNotFoundException ex => new CreateRouteRejected(ex.Message, ex.Code),
+                InvalidAggregateIdException ex => new CreateRouteRejected(ex.Message, ex.Code),
+                InvalidPointOrderException ex => new CreateRouteRejected(ex.Message, ex.Code),
+                InvalidRouteDescriptionException ex => new CreateRouteRejected(ex.Message, ex.Code),
+                InvalidRouteNameException ex => new CreateRouteRejected(ex.Message, ex.Code),
+                RouteDescriptionTooLongException ex => new CreateRouteRejected(ex.Message, ex.Code),
+                RouteNameTooLongException ex => new CreateRouteRejected(ex.Message, ex.Code),
+                RouteNameTooShortException ex => new CreateRouteRejected(ex.Message, ex.Code),
+            };
+    }
+}
