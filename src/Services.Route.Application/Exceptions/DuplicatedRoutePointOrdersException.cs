fa2826c998720d8d5fdd2bc42a2a6dffc85d6043
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-21 11:04:21 +0200

    Created exceptions

---
+++ src/Services.Route.Application/Exceptions/DuplicatedRoutePointOrdersException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Services.Route.Application.Exceptions
+{
+    public class DuplicatedRoutePointOrdersException : AppException
+    {
+        public override string Code { get; } = "route_points_duplicated_orders";
+        public Guid UserId { get; }
+
+        public DuplicatedRoutePointOrdersException(Guid userId)
+            : base($"User with id: {userId} has given points with duplicated orders.")
+        {
+            UserId = userId;
+        }
+    }
+}
---
+++ src/Services.Route.Application/Exceptions/RouteAlreadyExistsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Services.Route.Application.Exceptions
+{
+    public class RouteAlreadyExistsException: AppException
+    {       
+        public override string Code { get; } = "route_already_exists";
+        public Guid UserId { get; }
+        public string Name { get; }
+        
+        public RouteAlreadyExistsException(Guid userId, string name) 
+            : base($"User with id: {userId} has given existing route name: {name}.")
+
+        {
+            UserId = userId;
+            Name = name;
+        }
+    }
+}
