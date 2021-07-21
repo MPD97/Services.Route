babd36d13acaf42443f4439863112b645a977e94
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-21 11:02:27 +0200

    Updated entity

--- src/Services.Route.Core/Entities/AggregateRoot.cs
+++ src/Services.Route.Core/Entities/AggregateRoot.cs
@@ -3,20 +3,12 @@ using System.Linq;
 
 namespace Services.Route.Core.Entities
 {
-    public interface IAggregateRoot
-    {
-        IEnumerable<IDomainEvent> Events { get; }
-        AggregateId Id { get;  }
-        int Version { get; }
-        void IncrementVersion();
-    }
-    public abstract class AggregateRoot : IAggregateRoot
+    public abstract class AggregateRoot 
     {
         private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
         public IEnumerable<IDomainEvent> Events => _events;
         public AggregateId Id { get; protected set; }
         public int Version { get; protected set; }
-        public bool HasChanged => _events.Any();
 
         protected void AddEvent(IDomainEvent @event)
         {
@@ -24,8 +16,6 @@ namespace Services.Route.Core.Entities
         }
 
         public void ClearEvents() => _events.Clear();
-
-        void IAggregateRoot.IncrementVersion()
-            => Version++;
+        
     }
 }
