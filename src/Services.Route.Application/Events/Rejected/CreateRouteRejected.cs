48191209bbe51f87b91f75c347f24ee0ee67a7d6
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-23 12:33:05 +0200

    Updated event

--- src/Services.Route.Application/Events/Rejected/CreateRouteRejected.cs
+++ src/Services.Route.Application/Events/Rejected/CreateRouteRejected.cs
@@ -6,7 +6,6 @@ namespace Services.Route.Application.Events.Rejected
     [Contract]
     public class CreateRouteRejected : IRejectedEvent
     {
-        public Guid UserId { get; }
         public string Reason { get; }
         public string Code { get; }
 
