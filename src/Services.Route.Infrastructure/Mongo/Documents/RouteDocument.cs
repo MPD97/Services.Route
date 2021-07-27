0928e0d051c750bddee9ac40c04da72b882cbe2c
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-27 15:05:58 +0200

    Updated document

--- src/Services.Route.Infrastructure/Mongo/Documents/RouteDocument.cs
+++ src/Services.Route.Infrastructure/Mongo/Documents/RouteDocument.cs
@@ -8,23 +8,16 @@ namespace Services.Route.Infrastructure.Mongo.Documents
     public sealed class RouteDocument : IIdentifiable<Guid>
     {
         public Guid Id { get; set; }
-        
         public Guid UserId { get; set; }
-        
         public Guid? AcceptedBy { get; set; }
-        
         public string Name { get; set; }
-        
         public string Description { get; set; }
-        
         public Difficulty Difficulty { get; set; }
-
         public ActivityKind ActivityKind  { get; set; }
-        
         public int Length { get; set; }
-        
         public Status Status { get; set; }
-        
         public IEnumerable<PointDocument> Points { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
     }
 }
