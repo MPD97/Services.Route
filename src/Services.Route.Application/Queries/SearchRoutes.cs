511db329db0ad53fa5d7434f09680428afa5db9f
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-27 19:28:29 +0200

    Created route search

---
+++ src/Services.Route.Application/Queries/SearchRoutes.cs
@@ -0,0 +1,26 @@
+﻿using Convey.CQRS.Queries;
+using Services.Route.Application.DTO;
+using Services.Route.Core.Entities;
+
+namespace Services.Route.Application.Queries
+{
+    public class SearchRoutes : PagedQueryBase, IQuery<PagedResult<RouteDto>>
+    {
+        public Difficulty? Difficulty { get; set; } 
+        public ActivityKind? ActivityKind { get; set; }
+        public decimal? TopLeftLatitude { get; set; }
+        public decimal? TopLeftLongitude { get; set; }
+        public decimal? BottomRightLatitude { get; set; }
+        public decimal? BottomRightLongitude { get; set; }
+
+        public SearchRoutes(Difficulty? difficulty, ActivityKind? activityKind, decimal? topLeftLatitude, decimal? topLeftLongitude, decimal? bottomRightLatitude, decimal? bottomRightLongitude)
+        {
+            Difficulty = difficulty;
+            ActivityKind = activityKind;
+            TopLeftLatitude = topLeftLatitude;
+            TopLeftLongitude = topLeftLongitude;
+            BottomRightLatitude = bottomRightLatitude;
+            BottomRightLongitude = bottomRightLongitude;
+        }
+    }
+}
---
+++ src/Services.Route.Infrastructure/Mongo/Queries/Handlers/SearchRoutesHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using Services.Route.Application.DTO;
+using Services.Route.Application.Queries;
+using Services.Route.Core.Entities;
+using Services.Route.Infrastructure.Mongo.Documents;
+
+namespace Services.Route.Infrastructure.Mongo.Queries.Handlers
+{
+    public class SearchRoutesHandler : IQueryHandler<SearchRoutes, PagedResult<RouteDto>>
+    {
+        private readonly IMongoRepository<RouteDocument, Guid> _repository;
+        private static readonly int MaxPossibleActivityKind = Enum.GetValues(typeof(ActivityKind)).Cast<int>().Sum();
+
+        public SearchRoutesHandler(IMongoRepository<RouteDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<PagedResult<RouteDto>> HandleAsync(SearchRoutes query)
+        {
+            Expression<Func<RouteDocument, bool>> predicate = _ => true ;
+
+            if (query.Difficulty.HasValue)
+                predicate = predicate.And(r => r.Difficulty == query.Difficulty.Value);
+
+            if (query.ActivityKind.HasValue && query.ActivityKind > 0 && (int)query.ActivityKind <= MaxPossibleActivityKind)
+                predicate = predicate.And(r => r.ActivityKind == query.ActivityKind);
+
+            if (query.TopLeftLatitude.HasValue && query.BottomRightLatitude.HasValue)
+                predicate = predicate.And(r 
+                    => r.Latitude <= query.TopLeftLatitude && r.Latitude >= query.BottomRightLatitude);
+
+            if (query.TopLeftLongitude.HasValue && query.BottomRightLongitude.HasValue)
+                predicate = predicate.And(r
+                    => r.Longitude >= query.TopLeftLongitude && r.Longitude <= query.BottomRightLongitude);
+            
+            var pagedResult = await _repository.BrowseAsync(predicate, query);
+            return pagedResult?.Map(d => d.AsDto());        
+        }
+    }
+}
